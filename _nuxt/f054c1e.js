b054112380cb5d2875e5f7943d2257e3b84507fe
r-melnik <r-melnik@users.noreply.github.com>
2023-09-13 18:44:08 +0000

    deploy: ec4eab4f2bb2f906bf44a2c6ef16bd892ec2fc1e

--- _nuxt/f054c1e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{496:function(e,n,t){"use strict";t.r(n);var c={components:{GenericPage:t(220).a}},o=t(18),component=Object(o.a)(c,(function(){return(0,this._self._c)("GenericPage",{attrs:{path:"/about/team"}})}),[],!1,null,null,null);n.default=component.exports}}]);
