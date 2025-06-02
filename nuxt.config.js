51e22aae1124947851dc0ac0f43e54913755ec5d
Arman <>
2025-06-02 04:24:07 -0400

    june 2025 update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,32 +1,28 @@
-import pages from "./static/data/pages.json";
 import books from "./static/data/books.json";
-import proceedings from "./static/data/proceedings.json";
 import journals from "./static/data/journals.json";
+import pages from "./static/data/pages.json";
+import proceedings from "./static/data/proceedings.json";
 import projects from "./static/data/projects.json";
 const getLinks = async () => {
   var links = [];
-  Object.keys(pages).forEach(key => {
+  Object.keys(pages).forEach((key) => {
     if (!key.match(/\[[^\]]*\]/g)) {
       links.push(key);
     }
   });
-  [
-    ...books,
-    ...proceedings,
-    ...journals,
-  ].forEach(publication => {
-    links.push('/research/publications/' + publication.shortcode)
-  })
-  projects.forEach(project => {
-    links.push('/research/projects/' + project.id)
+  [...books, ...proceedings, ...journals].forEach((publication) => {
+    links.push("/research/publications/" + publication.shortcode);
+  });
+  projects.forEach((project) => {
+    links.push("/research/projects/" + project.id);
     if (project && project.subprojects) {
-      project.subprojects.forEach(subproject => {
-        links.push('/research/projects/' + project.id + '/' + subproject.id)
-      })
+      project.subprojects.forEach((subproject) => {
+        links.push("/research/projects/" + project.id + "/" + subproject.id);
+      });
     }
-  })
+  });
   return links;
-}
+};
 export default {
   ssr: false,
   target: "static",
@@ -36,11 +32,11 @@ export default {
   router: {
     scrollBehavior: function (to, from, savedPosition) {
       return { x: 0, y: 0 };
-    }
+    },
   },
   head: {
     htmlAttrs: {
-      lang: "en"
+      lang: "en",
     },
     meta: [
       { charset: "utf-8" },
@@ -52,37 +48,33 @@ export default {
         hid: "robots",
         name: "robots",
         content: "all",
-      }
+      },
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
         rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900"
+        href: "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900",
       },
 
       {
         rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Architects+Daughter|PT+Serif&display=swap"
+        href: "https://fonts.googleapis.com/css?family=Architects+Daughter|PT+Serif&display=swap",
       },
 
       {
         rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Noto+Serif:400i&display=swap"
+        href: "https://fonts.googleapis.com/css?family=Noto+Serif:400i&display=swap",
       },
       {
         rel: "stylesheet",
-        href: "https://cdn.quilljs.com/1.3.6/quill.snow.css"
+        href: "https://cdn.quilljs.com/1.3.6/quill.snow.css",
       },
       {
         rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
-      }
-    ]
+        href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons",
+      },
+    ],
   },
   /*
    ** Customize the progress-bar color
@@ -109,19 +101,19 @@ export default {
     "vue-scrollto/nuxt",
     "@nuxtjs/robots",
     "@nuxtjs/sitemap",
-    "@nuxtjs/svg"
+    "@nuxtjs/svg",
   ],
   sitemap: {
     hostname: "https://r-melnik.github.io",
     gzip: true,
-    routes: getLinks
+    routes: getLinks,
   },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseUrl: 'https://r-melnik.github.io'
+    baseUrl: "https://r-melnik.github.io",
   },
   /*
    ** vuetify module configuration
@@ -130,16 +122,16 @@ export default {
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
     theme: {
-      dark: false
-    }
+      dark: false,
+    },
   },
   router: {},
   server: {},
   generate: {
     async routes(callback) {
-      const links = await getLinks()
+      const links = await getLinks();
       callback(null, links);
-    }
+    },
   },
   /*
    ** Build configuration
@@ -150,6 +142,6 @@ export default {
       if (isClient) {
         config.optimization.splitChunks.maxSize = 500000;
       }
-    }
-  }
+    },
+  },
 };
